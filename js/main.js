24b45c6c66b4d1a60dee3ae5768e3b25be01f5c7
Alexandra Haley <haley9579@student.cptc.edu>
2021-04-15 14:56:49 -0700

    added main javascript file.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    let formBTN = document.querySelector("form > button");
+    formBTN.onclick = main;
+};
+function main() {
+    alert("reg button was clicked");
+}
