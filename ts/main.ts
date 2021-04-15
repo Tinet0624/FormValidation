43b5277a1399f2c3a84a4b5f5327f280aad772d3
Alexandra Haley <haley9579@student.cptc.edu>
2021-04-15 15:26:45 -0700

    Added form validation

--- ts/main.ts
+++ ts/main.ts
@@ -5,5 +5,25 @@ window.onload = function(){
 }
 
 function main(): void{
-    alert("reg button was clicked");
+    validateTxtInput();
+
+}
+/**
+ * // Return true if the text boxes with the 
+ * given ID has some text inside it.
+ * @param id the id of the <input type="text"> to validate.
+ * @param errMsg message to display in the sibling span of the textbox
+ * @returns true or false
+ */
+function validateTxtInput(id:string, errMsg:string):boolean{
+
+    let txtBox = <HTMLInputElement>document.getElementById(id);
+    let txtBoxValue = txtBox.value;
+
+    if(txtBoxValue == ""){
+        let errSpan = <HTMLElement>txtBox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
